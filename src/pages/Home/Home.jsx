3a9de302e21439cae4a8307ff0ab84ecc227997d
Kostya-Romanovskyi <romanovskiyk97@gmail.com>
2023-10-23 19:59:14 +0300

    added header, routes

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import { Container } from "./Home.styled";
+
+const Home = () => {
+  return (
+    <>
+      <Container className="container">
+        <h1>Home page</h1>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
