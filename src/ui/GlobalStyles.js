1dce61ab2ce746a714d7d4ab38f25be91cdab01f
Kostya-Romanovskyi <romanovskiyk97@gmail.com>
2023-10-23 19:03:08 +0300

    added fonts, modern normilize, global styles

---
+++ src/ui/GlobalStyles.js
@@ -0,0 +1,79 @@
+import { createGlobalStyle } from "styled-components";
+import { media } from "../ui/media";
+
+const GlobalStyles = createGlobalStyle`
+
+@font-face {
+    font-family: 'Grtsk Peta';
+    src: url('./fonts/GrtskPeta-Regular.eot');
+        url('./fonts/GrtskPeta-Regular.eot?#iefix') format('embedded-opentype'),
+        url('./fonts/GrtskPeta-Regular.woff2') format('woff2'),
+        url('./fonts/GrtskPeta-Regular.woff') format('woff'),
+        url('./fonts/GrtskPeta-Regular.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'Grtsk Peta';
+    src: url('../fonts/GrtskPeta-Bold.eot');
+
+        url('../fonts/GrtskPeta-Bold.eot?#iefix') format('embedded-opentype'),
+        url('../fonts/GrtskPeta-Bold.woff2') format('woff2'),
+        url('../fonts/GrtskPeta-Bold.woff') format('woff'),
+        url('../fonts/GrtskPeta-Bold.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: 'Grtsk Peta';
+    src: url('../fonts/GrtskPeta-Medium.eot');
+    src: local('Grtsk Peta Medium'), local('GrtskPeta-Medium'),
+        url('../fonts/GrtskPeta-Medium.eot?#iefix') format('embedded-opentype'),
+        url('../fonts/GrtskPeta-Medium.woff2') format('woff2'),
+        url('../fonts/GrtskPeta-Medium.woff') format('woff'),
+        url('../fonts/GrtskPeta-Medium.ttf') format('truetype');
+    font-weight: 500;
+    font-style: normal;
+}
+
+h1,h2,h3,h4,h5,h6,p{
+    margin: 0;
+    padding: 0;
+}
+
+img{
+    display: block;
+}
+
+ul{
+    list-style: none
+    margin: 0;
+    padding: 0
+}
+
+body{
+    font-family: 'Grtsk Peta', sans-serif;
+}
+
+
+.container{
+    padding: 0 15px;
+
+    ${media.mobile} {
+        max-width: 480px;
+        margin: 0 auto;
+      }
+    ${media.tablet} {
+        max-width: 768px;
+        padding: 0 17px;
+      }
+    ${media.desktop} {
+        max-width: 1200px;
+        padding: 0 25px;
+      }
+}
+`;
+
+export default GlobalStyles;
---
+++ src/ui/breakpoints.js
@@ -0,0 +1,11 @@
+// export const Breakpoints = {
+//   SM: 415,
+//   MD: 820,
+//   XL: 1920,
+// };
+
+export const Breakpoints = {
+  SM: 480,
+  MD: 768,
+  XL: 1200,
+};
